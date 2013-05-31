eaf4175dcf39facb6c417b584e12368b324cf969
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2013-05-31 14:56:54 +0900

    add hourse_tour

---
+++ hourses_tour.rb
@@ -0,0 +1,15 @@
+module HoursesTour
+  class Solver
+    def initialize(a)
+    end
+
+    def can_move?(a, b, c, d)
+    end
+
+    def movable_positions(x, y)
+    end
+
+    def solve(x, y)
+    end
+  end
+end

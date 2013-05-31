7eb423ea0814aec8943f7294def1607eeebf2c16
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2013-05-31 17:59:45 +0900

    hourses_tourが動いたような...

--- hourses_tour.rb
+++ hourses_tour.rb
@@ -1,3 +1,5 @@
+require './utility.rb'
+
 module HoursesTour
   class Solver
     def initialize(grid)
@@ -5,6 +7,24 @@ module HoursesTour
     end
 
     def solve(x, y)
+      @path = []
+      @solutions = []
+      solve_with_backtracking(x, y, 1)
+      @solutions
+    end
+
+    def solve_with_backtracking(x, y, k)
+      @path << [x, y]
+      if k == 9
+        @solutions << @path.clone if can_move?(x, y, @path[0][0], @path[0][1])
+      else
+        movable_positions(x, y).each{|nx, ny|
+          if @grid.cell(nx, ny).to_i == k+1
+            solve_with_backtracking(nx, ny, k+1)
+          end
+        }
+      end
+      @path.pop
     end
 
     def can_move?(sx, sy, tx, ty)
@@ -15,43 +35,47 @@ module HoursesTour
     def movable_positions(x, y)
       result = Set[]
       # 上
-      result << [x, y - 1]
+      result << [x, y - 1] if (y - 1) > 0
       # 右
-      result << [x - 1, y]
+      result << [x - 1, y] if (x - 1) > 0
       # 下
-      result << [x, y + 1]
+      result << [x, y + 1] if (y + 1) <= 9
       # 左
-      result << [x + 1, y]
+      result << [x + 1, y] if (x + 1) <= 9
       # 左下
       for i in x..9
         yy = y + (i - x)
-        if y <= 9
+        if yy <= 9
           result << [i, yy]
         end
       end
       # 右上
       for i in 1..x
         yy = y - (x - i)
-        if y >= 1
+        if yy >= 1
           result << [i, yy]
         end
       end
       # 左上
       for i in x..9
         yy = y - (i - x)
-        if y >= 1
+        if yy >= 1
           result << [i, yy]
         end
       end
       # 右下
       for i in 1..x
         yy = y + (x - i)
-        if y <= 9
+        if yy <= 9
           result << [i, yy]
         end
       end
       
-      result - Set[[x, y]]
+      result = result - Set[[x, y]]
+
+      # puts "mp(#{x}, #{y}): #{ result.each.to_a }"
+
+      result
     end
 
 

1dfed90e0760834eceedd8669874f26b0eaf4e56
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2013-05-31 14:37:13 +0900

    testがひととおり通ったぞー

--- number_place.rb
+++ number_place.rb
@@ -8,16 +8,47 @@ module NumberPlace
     end
 
     def solve
+      solve_with_backtracking
+      @grid
+    end
+
+    def solve_simple
       for x in 1..9
         for y in 1..9
+          next if @grid.cell(x, y).to_i > 0
           candidates = list_candidates(x, y)
-          unless candidates.empty?
+          if candidates.empty?
+            # do nothing
+          elsif candidates.size == 1
             n = candidates.first
             @grid.set_cell(x, y, n)
+          else
+            # do nothing
           end
         end
       end
       @grid
+    end
+
+    def solve_with_backtracking
+      solve_simple()                  # まず、答が確定するところは解き進めます
+
+      next_zero = @grid.index("0")
+      return true if next_zero.nil?   # もう0が残っていない＝解答発見
+
+      # 0 のマスに対して、候補を一つずつ仮置きしてみます
+      x, y = @grid.index2pos(next_zero)
+      list_candidates(x, y).each{|k|
+        saved_grid = @grid.clone      # 盤面を保存しておく
+        @grid.set_cell(x, y, k)       # 数字を仮置きする
+
+        if solve_with_backtracking()
+          return true                 # 答が見つかったら盤面を @grid に残したままで終了
+        end
+
+        @grid = saved_grid            # 汚れた盤面を保存した状態に戻す
+      }
+      return false                    # 答が見つからなかった
     end
 
     def list_candidates(x, y)

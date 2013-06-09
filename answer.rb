2184cd83d938b912f8083574d616b5052c1b881b
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2013-06-09 23:15:02 +0900

    裏ルートみつけた!!

--- answer.rb
+++ answer.rb
@@ -3,4 +3,4 @@ require './fifth_page.rb'
 require './ordinals.rb'
 answer = $grid.split('').values_at(*$ordinals[0..21]).each_slice(2).map {|x,y| $plain_text.cell(x, y) }.join
 puts answer
-
+puts decode the answer

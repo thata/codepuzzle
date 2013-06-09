3091b0339c83d0c93353081a78e6f12e7b8db4d7
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2013-06-09 16:59:19 +0900

    表クリア

--- answer.rb
+++ answer.rb
@@ -3,8 +3,7 @@ require './fifth_page.rb'
 require './ordinals.rb'
 answer = $grid.split('').values_at(*$ordinals[0..21]).each_slice(2).
   map do |x,y|
-    # ここは見えない
-    $grid.cell(x, y)
+    $plain_text.cell(x, y)
   end.join
 puts answer
 

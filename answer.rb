9e740eb639ff2e20ee2c94224b51c2681131be5d
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2013-06-09 22:29:56 +0900

    コードを整形

--- answer.rb
+++ answer.rb
@@ -1,9 +1,6 @@
 require './third_page.rb'
 require './fifth_page.rb'
 require './ordinals.rb'
-answer = $grid.split('').values_at(*$ordinals[0..21]).each_slice(2).
-  map do |x,y|
-    $plain_text.cell(x, y)
-  end.join
+answer = $grid.split('').values_at(*$ordinals[0..21]).each_slice(2).map {|x,y| $plain_text.cell(x, y) }.join
 puts answer
 
